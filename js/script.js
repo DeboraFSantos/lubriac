81bb5a4fbc1e0686d76f6426ed6c9a45658b4248
Débora Ferreira <debora.ferreira.s@hotmail.com>
2020-07-10 00:11:26 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-
+  $('.slider').slick({
+    autoplay:true,
+    draggable: true,
+    arrows: true,
+    dots: false,
+    fade: true,
+    speed: 900,
+    infinite: true,
+    pauseOnScroll:false,
+    pauseOnFocus:false,
+    pauseOnHover:false,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+    touchThreshold: 100
+  })
